8fb65d9e0d3a036e57e08f5aeeed053a1a2b1fea
Rakeshyelledi <rakeshyelledi@gmail.com>
2020-12-19 17:33:46 +0530

    Django Day25 project programs

---
+++ Day-25/DailyWorkStatus/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from DailyWorkStatus import views
+from django.contrib.auth import views as v
+
+urlpatterns = [
+	path("",views.home,name="hm"),
+	path("abt/",views.about,name="ab"),
+	path("cnt/",views.contact,name="ct"),
+	path("rg/",views.register,name="reg"),
+	path("dsh/",views.dashboard,name="dash"),
+	path("pf/",views.profile,name="pfe"),
+	path("upf/",views.updf,name="upfe"),
+	path("lg/",v.LoginView.as_view(template_name="html/login.html"),name="logi"),
+	path("lgo/",v.LogoutView.as_view(template_name="html/logout.html"),name="lgot"),
+]
